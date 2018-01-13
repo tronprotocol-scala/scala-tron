5ef02dded54bb6c7ff74a4cb66aff14fbab1c1ec
Roy van Kaathoven <ik@royvankaathoven.nl>
2018-01-13 12:41:13 +0100

    add simple test

--- tron-protocol/src/main/scala/org/tron/wallet/Wallet.scala
+++ tron-protocol/src/main/scala/org/tron/wallet/Wallet.scala
@@ -1,5 +1,8 @@
 package org.tron.wallet
 
 import org.tron.core.PublicKey
+import org.tron.crypto.ECKey
+import org.tron.utils.Utils
 
-case class Wallet(address: PublicKey)
+case class Wallet(
+  address: PublicKey = PublicKey(new ECKey(Utils.getRandom).getAddress))
