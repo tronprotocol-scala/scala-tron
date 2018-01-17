0471bda0233d5c4a6a519775fef5f111fc98cc53
Roy van Kaathoven <ik@royvankaathoven.nl>
2018-01-17 23:17:31 +0100

    add balance api

--- tron-protocol/src/main/scala/org/tron/peer/PeerBuilder.scala
+++ tron-protocol/src/main/scala/org/tron/peer/PeerBuilder.scala
@@ -4,7 +4,7 @@ import javax.inject.Inject
 
 import org.tron.core._
 import org.tron.storage.DbFactory
-import org.tron.utils.ByteArray
+import org.tron.utils.{ByteArray, KeyUtils}
 import org.tron.wallet.Wallet
 
 
@@ -20,6 +20,15 @@ class PeerBuilder @Inject() (
 
     // Build the blockchain
     val blockchain = new BlockchainImpl(dbFactory.buildOrCreate(Constant.BLOCK_DB_NAME))
+    if (blockchain.lastHash == null) {
+
+      val sender = KeyUtils.newKey
+
+      println("Adding genesis for address", ByteArray.toHexString(sender.ecKey.getPubKey))
+
+      blockchain.addGenesisBlock(sender.hex)
+    }
+
 
     val utxoSet = new UTXOSet(dbFactory.build(Constant.TRANSACTION_DB_NAME), blockchain)
     utxoSet.reindex()
