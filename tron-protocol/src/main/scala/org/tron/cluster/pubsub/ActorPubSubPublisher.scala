01ff8fc9a5d8b24dabf56a0ad608093cedc4373e
Roy van Kaathoven <ik@royvankaathoven.nl>
2018-01-19 20:35:45 +0100

    add missing files

---
+++ tron-protocol/src/main/scala/org/tron/cluster/pubsub/ActorPubSubPublisher.scala
@@ -0,0 +1,32 @@
+package org.tron.cluster.pubsub
+
+import javax.inject.{Inject, Singleton}
+
+import akka.actor.ActorSystem
+import akka.cluster.pubsub.DistributedPubSub
+import akka.cluster.pubsub.DistributedPubSubMediator.Publish
+
+@Singleton
+class ActorPubSubPublisher @Inject()(system: ActorSystem) {
+
+  private val mediator = DistributedPubSub(system).mediator
+
+  /**
+    * Publishes an event for the given name
+    *
+    * @param eventName name of the even to publish
+    * @param message payload
+    */
+  def publish(eventName: String, message: Any) = {
+    mediator ! Publish(eventName, message)
+  }
+
+  /**
+    * Publishes an event for the given name
+    */
+  def publishToGroup(eventName: String, message: Any) = {
+    mediator ! Publish(
+      topic = eventName,
+      msg = message)
+  }
+}
