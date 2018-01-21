2abb8a5b82bebc0bb6ab7f47b2ed80eb708de1cf
Timo <napokue@hotmail.com>
2018-01-21 10:00:42 -0800

    revert implicit class ByteStringImplicits; it was used after all

--- tron-protocol/src/main/scala/org/tron/utils/ByteStringUtils.scala
+++ tron-protocol/src/main/scala/org/tron/utils/ByteStringUtils.scala
@@ -3,6 +3,10 @@ package org.tron.utils
 import com.google.protobuf.ByteString
 
 object ByteStringUtils {
+  implicit class ByteStringImplicits(byteString: ByteString) {
+    def hex = ByteArrayUtils.toHexString(byteString.toByteArray)
+  }
+
   implicit def bytesToByteString(bytes: Array[Byte]) =
     ByteString.copyFrom(bytes)
 
