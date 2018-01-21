133ee8c68b2d3d19ea5065d2e6443909d67e49cc
Timo <napokue@hotmail.com>
2018-01-21 09:39:39 -0800

    start ByteStringUtilsSpec

---
+++ tron-protocol/src/test/scala/org/tron/utils/ByteStringUtilsSpec.scala
@@ -0,0 +1,11 @@
+package org.tron.utils
+
+import org.specs2.mutable._
+
+class ByteStringUtilsSpec extends Specification {
+  "ByteStringUtils Spec" should {
+    "bytesToByteString" in {
+
+    }
+  }
+}
