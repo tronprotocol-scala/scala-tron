04de4e18ee638d141d59f0262e70781247a1a027
Timo <napokue@hotmail.com>
2018-01-21 09:39:17 -0800

    remove implicit class ByteStringImplicits reformat ByteStringUtils

--- tron-protocol/src/main/scala/org/tron/utils/ByteStringUtils.scala
+++ tron-protocol/src/main/scala/org/tron/utils/ByteStringUtils.scala
@@ -3,12 +3,9 @@ package org.tron.utils
 import com.google.protobuf.ByteString
 
 object ByteStringUtils {
+  implicit def bytesToByteString(bytes: Array[Byte]) =
+    ByteString.copyFrom(bytes)
 
-  implicit class ByteStringImplicits(byteString: ByteString) {
-    def hex = ByteArrayUtils.toHexString(byteString.toByteArray)
-  }
-
-  implicit def bytesToByteString(bytes: Array[Byte]) = ByteString.copyFrom(bytes)
-  implicit def byteStringToBytes(byteString: ByteString) = byteString.toByteArray
-
+  implicit def byteStringToBytes(byteString: ByteString) =
+    byteString.toByteArray
 }
