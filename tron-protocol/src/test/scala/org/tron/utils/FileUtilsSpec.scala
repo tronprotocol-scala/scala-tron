3418ae561c348455c28aa65de062516beae6f5f9
Timo <napokue@hotmail.com>
2018-02-12 12:03:26 -0800

    add FileUtilsSpec

---
+++ tron-protocol/src/test/scala/org/tron/utils/FileUtilsSpec.scala
@@ -0,0 +1,17 @@
+package org.tron.utils
+
+import java.io.File
+import java.nio.file.Path
+
+import org.specs2.mutable._
+
+class FileUtilsSpec extends Specification {
+  "FileUtils Spec" should {
+    "recursiveList" in {
+      1 mustEqual 1
+    }
+    "recursiveDelete" in {
+      1 mustEqual 1
+    }
+  }
+}
